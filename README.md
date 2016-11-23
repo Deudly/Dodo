432ae994c6065900021a9a8fe01bc296d1cf8e9f
DeudlyYT <deudlymandame@gmail.com>
2016-11-23 21:35:11 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#Dodo Discord Bot
+
+Dodo Bot is a bot of Discord developer in NodeJS using Discord.js API
+
+## How to install and use?
+
+* Clone this repository
+* Open `main.js` and and write your Bot Key in `var key = "";`. Between the `""`
+* Run `npm install` in the repository cloned folder
+* Start it with `npm start`
---
+++ main.js
@@ -0,0 +1,33 @@
+var Discord = require('discord.js');
+const client = new Discord.Client();
+var key = "";
+
+client.login(key);
+
+client.on('ready', () => {
+  console.log('Dodo is ready!');
+});
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+  if (message.content == "dodo"){
+      message.reply("Hey, im just a dodo and here you have a selfie of me!")
+      randomDodo(message);
+  }
+
+});
+
+function randomDodo(message){
+    var num = Math.random();
+    if (num < 0.1){
+        message.reply(" http://www.defensacentral.com/ustedpregunta/data/articulos/como-era-el-pajaro-dodo/51210d91ce266.jpg");
+    }
+    else if (num < 0.2 && num > 0.1){
+        message.reply(" https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Dodo,_Natural_History_Museum,_London_2.JPG/1280px-Dodo,_Natural_History_Museum,_London_2.JPG");
+    }else if (num < 0.3 && num > 0.2){
+        message.reply(" http://www.ohnitsch.net/wordpress/wp-content/uploads/2008/07/080721_dodo.png");
+    }else if (num < 0.4 && num > 0.3){
+        message.reply(" http://vignette1.wikia.nocookie.net/los-inmortales/images/c/c4/Dodo.jpgs");
+    }
+}
